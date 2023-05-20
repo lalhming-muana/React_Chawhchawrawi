19ebeefc4ed6f9373e1c2fc4e53b8d79789f61f5
Muana <lalhming.muana1982@gmail.com>
2023-05-20 22:22:04 +0530

    jsx conditionals

--- test/src/App.js
+++ test/src/App.js
@@ -5,11 +5,14 @@ import './App.css';
 class App extends Component{
 
   render(){
+    const name ='john doe';
+    const loading = true;
 
   return (
     <React.Fragment>
       <h1> Hello from React </h1> 
       <h2>Goodbye</h2>
+      {loading ? <h4>loading ....</h4> : <h4>Hello {name}</h4>}
     </React.Fragment>
   );
 }
