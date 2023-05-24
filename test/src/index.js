188271e5948bd17c2cd6cd3b9ebe7db6f40f0b3c
Muana <lalhming.muana1982@gmail.com>
2023-05-24 21:54:43 +0530

    starting the basic content

--- test/src/index.js
+++ test/src/index.js
@@ -1,13 +1,24 @@
+/**
+ * To create a component we need a 5 step process
+ */
+
+// 1. import React and ReactDom
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+
+// 2. id="root" index.html ami reference lak tur
+const topEl = document.getElementById('root');
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// 3. A chunga mi hi React nen assign tur
+const root = ReactDOM.createRoot(topEl);
 
+
+// 4. Component siam
+function App(){
+  return <h1>Hi there, you lucky Dog</h1>
+
+}
+
+// 5. Component display
+root.render(<App />);
