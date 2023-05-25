fd285164d59f1e0c2bda7ebdf3696b18590cd9f1
Muana <lalhming.muana1982@gmail.com>
2023-05-25 20:34:48 +0530

    Section 2 is complete

--- test/src/index.js
+++ test/src/index.js
@@ -6,6 +6,7 @@
 // 1. import React and ReactDom
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
 
 // 2. id="root" index.html ami reference lak tur
 const topEl = document.getElementById('root');
@@ -14,22 +15,7 @@ const topEl = document.getElementById('root');
 const root = ReactDOM.createRoot(topEl);
 
 // 4. Component siam
-function App(){
 
-  let message = 'Hi there, you lucky Cow'; 
-  const secondMessage = 123; // hetah hian boolean, null , undefined leh object a hman theihloh
-
- const date = new Date();
- const time = date.toLocaleString();
-
-  return <div>
-            <h1>{message} {secondMessage} {time}</h1>
-            <input type="number" 
-              min={5} 
-              max={20}
-              style={{border: '3px solid red'}}/>
-         </div>  
-}
 
 // 5. Component display
 root.render(<App />);
