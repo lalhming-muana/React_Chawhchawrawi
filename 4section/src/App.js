d16199d08608d353ff631787dc45b30957169711
Muana <lalhming.muana1982@gmail.com>
2023-05-27 23:00:41 +0530

    Animals display app coimpleted

--- 4section/src/App.js
+++ 4section/src/App.js
@@ -1,17 +1,37 @@
 import AnimalShow from './AnimalShow';
 import {useState} from 'react';
+import './App.css'
+
+function getRandomAnimal(){
+  const animals =['bird','cat','cow','dog','gator','horse'];
+
+  return animals[Math.floor(Math.random()*animals.length)]
+}
+
 
 function App(){
 
-  const [count,setCount] = useState(0);
+  const [animals,setAnimals] = useState([]);
+  
   const handleClick=()=>{
-    setCount(count +1);
+    // A hnuaia array syntax hi kan hman nachhan chu a tira awmsa kha display reng kan duh vang ania. Ramsa lem indawt a tlar phei kan duh vang ani
+    setAnimals([...animals, getRandomAnimal()]);
   }
+  
+  // Hetah hian index hi chu a element pakhat index sawina kha ani.
+  // renderedAnimals hi array ani anga, element thar apiang a rawn in add
+  // belh zel ang. AnimalShow element hlir a awm ang.
+
+
+
+  const renderedAnimals = animals.map((animal, index)=>{
+    return <AnimalShow type={animal} key={index} />
+  })
 
   return (
-            <div>
+            <div className="app">
               <button onClick={handleClick}>Add Animal</button>
-              <div>Number of animals: {count} </div>
+              <div className="animal-list"> {renderedAnimals} </div>
             </div>
   )
 }
